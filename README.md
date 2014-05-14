68b7ea8dcd826346288fa94742be3647f30e35dd
Peter <fr0z3nk0@gmail.com>
2014-05-14 12:41:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Clone this in ~/.bash/prompt
+
+add this into bash_profile 
+export GITAWAREPROMPT=~/.bash/prompt
+source $GITAWAREPROMPT/main.sh
+
---
+++ prompt.sh
@@ -0,0 +1,36 @@
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then 
+ c_reset=`tput sgr0`
+  c_user=`tput setaf 2; tput bold`
+  c_path=`tput setaf 4; tput bold`
+  c_git_clean=`tput setaf 2`
+  c_git_dirty=`tput setaf 1`
+else
+c_reset=
+  c_user=
+  c_path=
+  c_git_clean=
+  c_git_dirty=
+fi
+
+git_prompt ()
+{
+  if ! git rev-parse --git-dir > /dev/null 2>&1; then
+return 0
+  fi
+
+
+git_branch=$(git branch 2>/dev/null| sed -n '/^\*/s/^\* //p')
+  
+ git_status="$(git status 2> /dev/null)"
+ 
+# Set color based on clean/staged/dirty.
+if [[ ${git_status} =~ "working directory clean" ]]; then
+  git_color="${c_git_clean}"
+else
+  git_color="${c_git_dirty}"
+fi
+
+echo " [$git_color$git_branch${c_reset}]"
+}
+
+PS1='\[\033[0;36m\]\d \t \[\e[0;31m\]\u\[\e[36;1m\]@\[\e[32;1m\]\H:\[\e[33;1m\]\w\[\e[32;1m\]\[\e[0m\]$(git_prompt)\[\e[36;1m\]\$ ${c_reset}'
